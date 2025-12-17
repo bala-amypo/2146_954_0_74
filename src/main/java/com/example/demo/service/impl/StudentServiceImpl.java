5c628f9f8a61268ff39a79808d95c35a4114efc3
bala-amypo <bala@amypo.in>
2025-12-17 05:13:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,34 +1,35 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import java.util.*;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.Student;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.service.Studentservice;
 
 @Service
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl implements Studentservice {
 
-    private final Map<Long, Student> store = new HashMap<>();
-    private long counter = 1;
+    private final Map<Integer, StudentEntity> store = new HashMap<>();
+    private int counter = 1;
 
     @Override
-    public Student insertStudent(Student st) {
+    public StudentEntity insertStudent(StudentEntity st) {
         st.setId(counter++);
         store.put(st.getId(), st);
         return st;
     }
 
     @Override
-    public List<Student> getAllStudents() {
+    public List<StudentEntity> getAllStudents() {
         return new ArrayList<>(store.values());
     }
 
     @Override
-    public Optional<Student> getOneStudent(Long id) {
+    public Optional<StudentEntity> getOneStudent(int id) {
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
-    public void deleteStudent(Long id) {
+    public void deleteStudent(int id) {
         store.remove(id);
     }
 }
