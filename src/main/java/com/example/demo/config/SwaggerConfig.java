6011a7c718d88a7b1bfb3399697157acff6a579c
bala-amypo <bala@amypo.in>
2025-12-18 09:35:40 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -5,12 +5,17 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    // Swagger OpenAPI config
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -19,9 +24,22 @@ public class SwaggerConfig {
                 ));
     }
 
+    // BCrypt encoder bean
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    // Temporary in-memory login user
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder encoder) {
+
+        UserDetails user = User.builder()
+                .username("root")
+                .password(encoder.encode("Amypo"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
