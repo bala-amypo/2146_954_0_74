e04dc652e3701e0924b356123f1ced1aad48142a
bala-amypo <bala@amypo.in>
2025-12-19 06:36:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.LocationEntity;
 import com.example.demo.repository.LocationRepository;
+import com.example.demo.service.LocationService;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class LocationServiceImpl {
+public class LocationServiceImpl implements LocationService {
 
     private final LocationRepository locationRepository;
 
@@ -14,11 +16,24 @@ public class LocationServiceImpl {
         this.locationRepository = locationRepository;
     }
 
+    @Override
     public LocationEntity saveLocation(LocationEntity location) {
         return locationRepository.save(location);
     }
 
+    @Override
     public List<LocationEntity> getAllLocations() {
         return locationRepository.findAll();
+    }
+
+    @Override
+    public LocationEntity getLocationById(Long id) {
+        Optional<LocationEntity> location = locationRepository.findById(id);
+        return location.orElse(null);
+    }
+
+    @Override
+    public void deleteLocation(Long id) {
+        locationRepository.deleteById(id);
     }
 }
